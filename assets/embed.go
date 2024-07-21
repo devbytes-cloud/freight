032c7fc59d1c056b8d6ba7d43dfcce34e5e18b68
David Dymko <dymkod@gmail.com>
2024-07-21 08:46:56 -0400

    added remaining assets

--- assets/embed.go
+++ assets/embed.go
@@ -9,3 +9,21 @@ var MacOSIntel []byte
 
 //go:embed dist/railcar_darwin_arm64/railcar
 var MacOSSilicon []byte
+
+//go:embed dist/railcar_linux_amd64_v1/railcar
+var LinuxAMD64 []byte
+
+//go:embed dist/railcar_linux_arm64/railcar
+var LinuxARM64 []byte
+
+//go:embed dist/railcar_linux_arm_6/railcar
+var LinuxARM32 []byte
+
+//go:embed dist/railcar_windows_amd64_v1/railcar.exe
+var WindowsAMD64 []byte
+
+//go:embed dist/railcar_windows_arm64/railcar.exe
+var WindowsARM64 []byte
+
+//go:embed dist/railcar_windows_arm_6/railcar.exe
+var WindowsARM32 []byte
--- internal/embed/embeder.go
+++ internal/embed/embeder.go
@@ -14,6 +14,12 @@ import (
 const (
 	macOSSilicon string = "darwin-arm64"
 	macOSIntel   string = "darwin-amd64"
+	linuxAMD64   string = "linux-amd64"
+	linuxARM64   string = "linux-arm64"
+	linuxARM32   string = "linux-arm"
+	windowsAMD64 string = "windows-amd64"
+	windowsARM64 string = "windows-arm64"
+	windowsARM32 string = "windows-arm"
 )
 
 // WriteBinary ...
@@ -39,6 +45,18 @@ func fetchBinary(systemInfo string) []byte {
 		return assets.MacOSSilicon
 	case macOSIntel:
 		return assets.MacOSIntel
+	case linuxAMD64:
+		return assets.LinuxAMD64
+	case linuxARM64:
+		return assets.LinuxARM64
+	case linuxARM32:
+		return assets.LinuxARM32
+	case windowsAMD64:
+		return assets.WindowsAMD64
+	case windowsARM64:
+		return assets.WindowsARM64
+	case windowsARM32:
+		return assets.WindowsARM32
 	default:
 		return nil
 	}
