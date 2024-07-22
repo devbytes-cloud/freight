6cd3efdfa1616b41ef9d8ce296de6c15137cc0d8
David Dymko <dymkod@gmail.com>
2024-07-22 11:34:10 -0400

    docs

--- assets/embed.go
+++ assets/embed.go
@@ -2,7 +2,7 @@ package assets
 
 import _ "embed"
 
-// This file is meant to embed the railcar into skiff. This allows skiff to install the railcar onto your machine
+// This file is meant to embed the railcar into freight. This allows freight to install the railcar onto your machine
 
 //go:embed dist/railcar_darwin_amd64_v1/railcar
 var MacOSIntel []byte
