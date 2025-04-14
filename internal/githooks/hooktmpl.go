6746af535b486b067fdca0b8aa7c2afca140a783
David Dymko <dymkod@gmail.com>
2025-04-13 20:24:32 -0400

    feat: commit msg input (#20)

--- internal/githooks/hooktmpl.go
+++ internal/githooks/hooktmpl.go
@@ -4,7 +4,4 @@ package githooks
 // The template includes placeholders for the hook type (`{{ .Type }}`) and path (`{{ .Path }}`),
 // which are replaced with actual values when the template is rendered.
 const gitHookTemplate = `#!/bin/bash
-echo "================================"
-echo "Railcar {{ .Type }} Hook"
-echo "================================"
 {{ .Path }}/conductor {{ .Type }} $1`
